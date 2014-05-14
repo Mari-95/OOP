15b58ef7709eb9e21197cea2a59116ba62d8b559
Mari-95 <pogosyan-mari@mail.ru>
2014-05-14 09:21:02 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+main ()
+{ 
+        Matrix m1 (3,3);
+        m1 SetCoef (1,1,5);
+        m1 SetCoef (1,2,6);
+        m1 SetCoef (1,4,7);
+        m2 SetCoef (2,1,4);
+        m2 SetCoef (3,2,1);
+        m2 SetCoef (4,2,3);
+        m3 SetCoef (1,5,3);
+        m3 SetCoef (2,7,1);
+        m3 SetCoef (1,5,4);
+        m1 Show ();
+        Matrix m2=m1;
+        
+        
